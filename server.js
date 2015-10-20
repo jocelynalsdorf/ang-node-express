d57065ef75cd60fd591a67364252fccd102f2926
jocelynalsdorf <jfrodsla@gmail.com>
2015-10-20 12:55:40 -0700

    add server.js and views

---
+++ server.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+
+//set the public folder to serve public assets like css, imgs or js (all frontend code)
+app.use(express.static(__dirname + '/public'));
+
+//set up our one route to the index.html file so that all requests get sent there
+app.get('*', function(req,res){
+  res.sendFile(path.join(__dirname + '/public/views/index.html'));
+
+});
+
+//start the server on port 8080 (http://localhost:8080)
+app.listen(8080);
+console.log('Magic happens on port 8080');
