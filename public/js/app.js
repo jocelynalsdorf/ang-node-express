7424487884d9c60e5434652ff84c67f2e8795e14
jocelynalsdorf <jfrodsla@gmail.com>
2015-10-21 11:10:44 -0700

    add ngRoutes and configure home, about, contact

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,24 @@
 //all angular code
+angular.module('routerApp', ['routerRoutes'])
+
+//create the controllers, first main controller for whole routerApp
+.controller('mainController', function(){
+  var vm = this;
+  vm.bigMessage ="Smooth seas don't make skilled sailors";
+
+})
+
+.controller('homeController', function(){
+  var vm = this;
+  vm.message ="this is the home page";
+})
+
+.controller('aboutController', function(){
+  var vm = this;
+  vm.message ="I am the about page";
+})
+
+.controller('contactController', function(){
+  var vm = this;
+  vm.message = " Contact me";
+});
--- public/js/app.routes.js
+++ public/js/app.routes.js
@@ -1,1 +1,30 @@
-//all angular routes
+angular.module('routerRoutes', ['ngRoute'])
+
+
+//configure routes
+.config(function($routeProvider, $locationProvider){
+  $routeProvider
+  //home route
+  .when('/', {
+    templateUrl: 'views/pages/home.html',
+    controller: 'homeController',
+    controllerAs: 'home'
+  })
+
+  //about route
+  .when('/about', {
+    templateUrl: 'views/pages/about.html',
+    controller: 'aboutController',
+    controllerAs: 'about'
+  })
+  //contact route
+  .when('/contact', {
+    templateUrl: 'views/pages/contact.html',
+    controller 'contactController',
+    controllerAs: 'contact'
+  });
+
+  //set app to have pretty urls (removes #) --you must have <base> set in head of the index.html for relative links too---
+  $locationProvider.html5Mode(true);
+
+});
